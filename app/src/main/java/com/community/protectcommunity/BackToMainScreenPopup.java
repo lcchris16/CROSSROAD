e614edf8081a631e2a79363be26aeaee6baf12a2
Chris Lee <45930387+lcchris16@users.noreply.github.com>
2019-04-07 12:35:37 +1000

    Update BackToMainScreenPopup.java

--- app/src/main/java/com/community/protectcommunity/BackToMainScreenPopup.java
+++ app/src/main/java/com/community/protectcommunity/BackToMainScreenPopup.java
@@ -20,8 +20,6 @@ public class BackToMainScreenPopup extends BasePopupWindow implements View.OnCli
         this.activity = activity;
     }
 
-    // 必须实现，这里返回您的contentView
-    // 为了让库更加准确的做出适配，强烈建议使用createPopupById()进行inflate
     @Override
     public View onCreateContentView() {
         popupWindow = createPopupById(R.layout.pop_up_window_back_to_mainscreen);
@@ -32,8 +30,6 @@ public class BackToMainScreenPopup extends BasePopupWindow implements View.OnCli
         return popupWindow;
     }
 
-    // 以下为可选代码（非必须实现）
-    // 返回作用于PopupWindow的show和dismiss动画，本库提供了默认的几款动画，这里可以自由实现
     @Override
     protected Animation onCreateShowAnimation() {
         return getDefaultScaleAnimation(true);
